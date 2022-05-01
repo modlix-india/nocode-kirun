977d3f1d196c92674f6ba4bc297dcde090365c51
Kiran Grandhi <kiran.grandhi@fincity.com>
2022-05-01 18:04:56 +0530

    Working on schema for schema

--- kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/Schema.java
+++ kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/Schema.java
@@ -1,5 +1,8 @@
 package com.fincity.nocode.kirun.engine.json.schema;
 
+import static com.fincity.nocode.kirun.engine.json.schema.type.SchemaType.*;
+import static java.util.Map.entry;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
@@ -9,11 +12,11 @@ import com.fincity.nocode.kirun.engine.json.schema.object.AdditionalPropertiesTy
 import com.fincity.nocode.kirun.engine.json.schema.string.StringFormat;
 import com.fincity.nocode.kirun.engine.json.schema.string.StringSchema;
 import com.fincity.nocode.kirun.engine.json.schema.type.SchemaType;
-import com.fincity.nocode.kirun.engine.json.schema.type.SingleType;
 import com.fincity.nocode.kirun.engine.json.schema.type.Type;
 import com.fincity.nocode.kirun.engine.model.FunctionSignature;
 import com.fincity.nocode.kirun.engine.namespaces.Namespaces;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -24,13 +27,43 @@ public class Schema implements Serializable {
 
 	private static final long serialVersionUID = 4041990622586726910L;
 
-	public static final Schema SCHEMA = new Schema()
-			.setNamespace(Namespaces.SYSTEM).setVersion(1)
+	public static final Schema SCHEMA = new Schema().setNamespace(Namespaces.SYSTEM).setVersion(1)
 			.setType(Type.of(SchemaType.OBJECT))
-			.setProperties(
-				"namespace", new Schema(). 
-			)
-			;
+			.setProperties(Map.ofEntries(
+					entry("namespace", Schema.of("namespace", STRING)),
+					entry("version", Schema.of("version", SchemaType.INTEGER)),
+					entry("ref", Schema.of("ref", STRING)),
+					entry("type", Schema.ofArray("type", new Schema().setType(Type.of(STRING))
+									.setEnums(List.of(new JsonPrimitive("INTEGER"), new JsonPrimitive("LONG"),
+											new JsonPrimitive("FLOAT"), new JsonPrimitive("DOUBLE"),
+											new JsonPrimitive("STRING"), new JsonPrimitive("OBJECT"),
+											new JsonPrimitive("ARRAY"), new JsonPrimitive("BOOLEAN"),
+											new JsonPrimitive("NULL"))))),
+					entry("anyOf", Schema.ofArray("anyOf", Schema.ofRef("#/"))),
+					entry("allOf", Schema.ofArray("allOf", Schema.ofRef("#/"))),
+					entry("oneOf", Schema.ofArray("oneOf", Schema.ofRef("#/"))),
+
+					entry("not", Schema.ofRef("#/")),
+					entry("title", Schema.of("title", STRING)),
+					entry("description", Schema.of("description", STRING)),
+					entry("id", Schema.of("id", STRING)),
+					
+					entry("examples",  Schema.ofArray("examples", Schema.of("example", INTEGER, LONG, FLOAT, DOUBLE, STRING, OBJECT, ARRAY, BOOLEAN, NULL))),
+					entry("defaultValue", Schema.of("defaultValue", INTEGER, LONG, FLOAT, DOUBLE, STRING, OBJECT, ARRAY, BOOLEAN, NULL))
+			));
+
+	public static Schema of(String id, SchemaType... type) {
+		return new Schema().setType(Type.of(type)).setId(id).setTitle(id);
+	}
+
+	public static Schema ofRef(String ref) {
+		return new Schema().setRef(ref);
+	}
+	
+	public static Schema ofArray(String id, Schema ...itemSchemas) {
+		return new Schema().setType(Type.of(SchemaType.ARRAY)).setId(id).setTitle(id)
+				.setItems(ArraySchemaType.of(itemSchemas));
+	}
 
 	private String namespace;
 
@@ -83,4 +116,6 @@ public class Schema implements Serializable {
 	private Integer maxItems;
 	private Boolean uniqueItems;
 	private List<FunctionSignature> methods;
+
+	private Map<String, Schema> definitions;
 }
--- kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/array/ArraySchemaType.java
+++ kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/array/ArraySchemaType.java
@@ -6,12 +6,22 @@ import java.util.List;
 import com.fincity.nocode.kirun.engine.json.schema.Schema;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class ArraySchemaType implements Serializable {
 
 	private static final long serialVersionUID = -3312223652146445370L;
-	
+
 	private Schema singleSchema;
 	private List<Schema> tupleSchema;
+
+	public static ArraySchemaType of(Schema... schemas) {
+
+		if (schemas.length == 1)
+			return new ArraySchemaType().setSingleSchema(schemas[0]);
+
+		return new ArraySchemaType().setTupleSchema(List.of(schemas));
+	}
 }
