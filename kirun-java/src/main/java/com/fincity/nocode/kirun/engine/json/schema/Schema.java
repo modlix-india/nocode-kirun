823eb12f8550c2a92fed59654b5f274db4681848
serialcoder13 <kiran.grandhi@fincity.com>
2022-06-13 13:36:07 +0530

    Type schema was wrong and now it is fixed.

--- kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/Schema.java
+++ kirun-java/src/main/java/com/fincity/nocode/kirun/engine/json/schema/Schema.java
@@ -62,64 +62,67 @@ public class Schema implements Serializable {
 	        .setType(Type.of(SchemaType.NULL))
 	        .setConstant(JsonNull.INSTANCE);
 
+	private static final Schema TYPE_SCHEMA = new Schema().setType(Type.of(SchemaType.STRING))
+	        .setEnums(List.of(new JsonPrimitive("INTEGER"), new JsonPrimitive("LONG"), new JsonPrimitive("FLOAT"),
+	                new JsonPrimitive("DOUBLE"), new JsonPrimitive("STRING"), new JsonPrimitive("OBJECT"),
+	                new JsonPrimitive("ARRAY"), new JsonPrimitive("BOOLEAN"), new JsonPrimitive("NULL")));
+
 	public static final Schema SCHEMA = new Schema().setNamespace(Namespaces.SYSTEM)
 	        .setName("Schema")
 	        .setType(Type.of(SchemaType.OBJECT))
-	        .setProperties(Map.ofEntries(
-	                entry(NAMESPACE_STRING, STRING), entry("name", STRING), entry(VERSION_STRING,
-	                        Schema.of(VERSION_STRING, SchemaType.INTEGER)
+	        .setProperties(
+	                Map.ofEntries(entry(NAMESPACE_STRING, STRING), entry("name", STRING),
+	                        entry(VERSION_STRING, Schema.of(VERSION_STRING, SchemaType.INTEGER)
 	                                .setDefaultValue(new JsonPrimitive(1))),
-	                entry("ref", STRING),
-	                entry("type", Schema.ofArray("type", new Schema().setType(Type.of(SchemaType.STRING))
-	                        .setEnums(List.of(new JsonPrimitive("INTEGER"), new JsonPrimitive("LONG"),
-	                                new JsonPrimitive("FLOAT"), new JsonPrimitive("DOUBLE"),
-	                                new JsonPrimitive("STRING"), new JsonPrimitive("OBJECT"),
-	                                new JsonPrimitive("ARRAY"), new JsonPrimitive("BOOLEAN"),
-	                                new JsonPrimitive("NULL"))))),
-	                entry("anyOf", Schema.ofArray("anyOf", Schema.ofRef(SHEMA_ROOT_PATH))),
-	                entry("allOf", Schema.ofArray("allOf", Schema.ofRef(SHEMA_ROOT_PATH))),
-	                entry("oneOf", Schema.ofArray("oneOf", Schema.ofRef(SHEMA_ROOT_PATH))),
+	                        entry("ref", STRING),
+	                        entry("type",
+	                                new Schema().setAnyOf(List.of(TYPE_SCHEMA, Schema.ofArray("type", TYPE_SCHEMA)))),
+	                        entry("anyOf", Schema.ofArray("anyOf", Schema.ofRef(SHEMA_ROOT_PATH))),
+	                        entry("allOf", Schema.ofArray("allOf", Schema.ofRef(SHEMA_ROOT_PATH))),
+	                        entry("oneOf", Schema.ofArray("oneOf", Schema.ofRef(SHEMA_ROOT_PATH))),
 
-	                entry("not", Schema.ofRef(SHEMA_ROOT_PATH)), entry("title", STRING), entry("description", STRING),
-	                entry("id", STRING), entry("examples", ANY), entry("defaultValue", ANY), entry("comment", STRING),
-	                entry("enums", Schema.ofArray("enums", STRING)), entry("constant", ANY),
+	                        entry("not", Schema.ofRef(SHEMA_ROOT_PATH)), entry("title", STRING),
+	                        entry("description", STRING), entry("id", STRING), entry("examples", ANY),
+	                        entry("defaultValue", ANY), entry("comment", STRING),
+	                        entry("enums", Schema.ofArray("enums", STRING)), entry("constant", ANY),
 
-	                entry("pattern", STRING), entry("format", Schema.of("format", SchemaType.STRING)
-	                        .setEnums(List.of(new JsonPrimitive("DATETIME"), new JsonPrimitive("TIME"),
-	                                new JsonPrimitive("DATE"), new JsonPrimitive("EMAIL"),
-	                                new JsonPrimitive("REGEX")))),
-	                entry("minLength", INTEGER), entry("maxLength", INTEGER),
+	                        entry("pattern", STRING), entry("format", Schema.of("format", SchemaType.STRING)
+	                                .setEnums(List.of(new JsonPrimitive("DATETIME"), new JsonPrimitive("TIME"),
+	                                        new JsonPrimitive("DATE"), new JsonPrimitive("EMAIL"),
+	                                        new JsonPrimitive("REGEX")))),
+	                        entry("minLength", INTEGER), entry("maxLength", INTEGER),
 
-	                entry("multipleOf", LONG), entry("minimum", NUMBER), entry("maximum", NUMBER),
-	                entry("exclusiveMinimum", NUMBER), entry("exclusiveMaximum", NUMBER),
+	                        entry("multipleOf", LONG), entry("minimum", NUMBER), entry("maximum", NUMBER),
+	                        entry("exclusiveMinimum", NUMBER), entry("exclusiveMaximum", NUMBER),
 
-	                entry("properties", Schema.of("properties", SchemaType.OBJECT)
-	                        .setAdditionalProperties(
-	                                new AdditionalPropertiesType().setSchemaValue(Schema.ofRef(SHEMA_ROOT_PATH)))),
-	                entry("additionalProperties",
-	                        Schema.of("additionalProperties", SchemaType.BOOLEAN, SchemaType.OBJECT)
-	                                .setRef(SHEMA_ROOT_PATH)),
-	                entry(REQUIRED_STRING, Schema.ofArray(REQUIRED_STRING, STRING)),
-	                entry("propertyNames", Schema.ofRef(SHEMA_ROOT_PATH)), entry("minProperties", INTEGER),
-	                entry("maxProperties", INTEGER), entry("patternProperties",
-	                        Schema.of("patternProperties", SchemaType.OBJECT)
+	                        entry("properties", Schema.of("properties", SchemaType.OBJECT)
+	                                .setAdditionalProperties(new AdditionalPropertiesType()
+	                                        .setSchemaValue(Schema.ofRef(SHEMA_ROOT_PATH)))),
+	                        entry("additionalProperties",
+	                                Schema.of("additionalProperties", SchemaType.BOOLEAN, SchemaType.OBJECT)
+	                                        .setRef(SHEMA_ROOT_PATH)),
+	                        entry(REQUIRED_STRING, Schema.ofArray(REQUIRED_STRING, STRING)),
+	                        entry("propertyNames", Schema.ofRef(SHEMA_ROOT_PATH)), entry("minProperties", INTEGER),
+	                        entry("maxProperties", INTEGER), entry("patternProperties",
+	                                Schema.of("patternProperties", SchemaType.OBJECT)
+	                                        .setAdditionalProperties(new AdditionalPropertiesType()
+	                                                .setSchemaValue(Schema.ofRef(SHEMA_ROOT_PATH)))),
+
+	                        entry(ITEMS_STRING, new Schema().setName(ITEMS_STRING)
+	                                .setTitle(ITEMS_STRING)
+	                                .setAnyOf(List.of(Schema.ofRef(SHEMA_ROOT_PATH)
+	                                        .setName("item")
+	                                        .setTitle("item"),
+	                                        Schema.ofArray("tuple", Schema.ofRef(SHEMA_ROOT_PATH))))),
+
+	                        entry("contains", Schema.ofRef(SHEMA_ROOT_PATH)), entry("minItems", INTEGER),
+	                        entry("maxItems", INTEGER), entry("uniqueItems", BOOLEAN),
+
+	                        entry("definitions", Schema.of("definitions", SchemaType.OBJECT)
 	                                .setAdditionalProperties(new AdditionalPropertiesType()
 	                                        .setSchemaValue(Schema.ofRef(SHEMA_ROOT_PATH)))),
 
-	                entry(ITEMS_STRING, new Schema().setName(ITEMS_STRING)
-	                        .setTitle(ITEMS_STRING)
-	                        .setAnyOf(List.of(Schema.ofRef(SHEMA_ROOT_PATH)
-	                                .setName("item")
-	                                .setTitle("item"), Schema.ofArray("tuple", Schema.ofRef(SHEMA_ROOT_PATH))))),
-
-	                entry("contains", Schema.ofRef(SHEMA_ROOT_PATH)), entry("minItems", INTEGER),
-	                entry("maxItems", INTEGER), entry("uniqueItems", BOOLEAN),
-
-	                entry("definitions", Schema.of("definitions", SchemaType.OBJECT)
-	                        .setAdditionalProperties(
-	                                new AdditionalPropertiesType().setSchemaValue(Schema.ofRef(SHEMA_ROOT_PATH)))),
-
-	                entry("permission", STRING)))
+	                        entry("permission", STRING)))
 	        .setRequired(List.of(NAMESPACE_STRING, VERSION_STRING));
 
 	public static Schema of(String id, SchemaType... types) {
