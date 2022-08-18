594fb0b46f158cf4bfc2f6064af6093263be1c16
serialcoder13 <kiran.grandhi@fincity.com>
2022-08-18 23:53:13 +0530

    TS Updates

--- kirun-js/src/engine/runtime/StatementMessage.ts
+++ kirun-js/src/engine/runtime/StatementMessage.ts
@@ -1,22 +1,22 @@
 import { StatementMessageType } from './StatementMessageType';
 
 export class StatementMessage {
-    private messageType: StatementMessageType;
-    private message: string;
+    private messageType?: StatementMessageType;
+    private message?: string;
 
     public constructor(messageType?: StatementMessageType, message?: string) {
         this.message = message;
         this.messageType = messageType;
     }
 
-    public getMessageType(): StatementMessageType {
+    public getMessageType(): StatementMessageType | undefined {
         return this.messageType;
     }
     public setMessageType(messageType: StatementMessageType): StatementMessage {
         this.messageType = messageType;
         return this;
     }
-    public getMessage(): string {
+    public getMessage(): string | undefined {
         return this.message;
     }
     public setMessage(message: string): StatementMessage {
