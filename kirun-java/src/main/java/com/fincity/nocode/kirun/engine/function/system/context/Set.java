ecd6c13492780e1a9875a9b36b42d86415d7e3f4
serialcoder13 <kiran.grandhi@fincity.com>
2022-08-22 22:29:17 +0530

    Set Function Java is fixed to add deep path objects to context.

--- kirun-java/src/main/java/com/fincity/nocode/kirun/engine/function/system/context/Set.java
+++ kirun-java/src/main/java/com/fincity/nocode/kirun/engine/function/system/context/Set.java
@@ -30,6 +30,7 @@ import com.fincity.nocode.kirun.engine.util.string.StringFormatter;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
 import reactor.util.function.Tuple2;
@@ -120,116 +121,104 @@ public class Set extends AbstractFunction {
 		}
 
 		JsonElement el = ce.getElement();
+		JsonElement prev = el;
+		Operation op = ops.removeLast();
+		ExpressionToken token;
+		JsonPrimitive mem = null;
 
-		while (ops.size() > 1) {
+		while (ops.size() > 1 && el != null) {
 
-			if (el == null) {
-				throw new KIRuntimeException(StringFormatter.format("Unable to set the context in the path $", key));
-			}
+			prev = el;
+			op = ops.removeLast();
+			token = tokens.removeLast();
 
-			Operation op = ops.removeLast();
-			ExpressionToken token = tokens.removeLast();
 			if (op == Operation.OBJECT_OPERATOR) {
 
 				if (!el.isJsonObject()) {
 					throw new KIRuntimeException(StringFormatter.format("$ has no object in the context", key));
 				}
 
-				String mem = null;
-				if (token instanceof ExpressionTokenValue etv)
-					mem = etv.getTokenValue()
-					        .getAsString();
-				else
-					mem = token.getExpression();
+				mem = token instanceof ExpressionTokenValue etv ? etv.getElement()
+				        .getAsJsonPrimitive() : new JsonPrimitive(token.getExpression());
 
 				el = el.getAsJsonObject()
-				        .get(mem);
+				        .get(mem.getAsString());
 			} else {
-				JsonElement je = token instanceof ExpressionTokenValue etv ? etv.getElement()
-				        : new JsonPrimitive(token.getExpression());
-
-				if (!je.isJsonPrimitive()) {
-					throw new KIRuntimeException(
-					        StringFormatter.format("Cannot extract json with key $ from the object $", je, el));
-				}
+				mem = token instanceof ExpressionTokenValue etv ? etv.getElement()
+				        .getAsJsonPrimitive() : new JsonPrimitive(token.getExpression());
 
 				if (el.isJsonArray()) {
 
-					Tuple2<SchemaType, Object> prim = PrimitiveUtil.findPrimitive(je);
+					Tuple2<SchemaType, Object> prim = PrimitiveUtil.findPrimitive(mem);
 
 					if (prim.getT1() != SchemaType.INTEGER || prim.getT1() != SchemaType.LONG) {
 						throw new KIRuntimeException(
-						        StringFormatter.format("Expecting a numerical index but found $", je));
+						        StringFormatter.format("Expecting a numerical index but found $", mem));
 					}
 
 					int index = ((Number) prim.getT2()).intValue();
 					JsonArray ja = el.getAsJsonArray();
-					if (index >= ja.size())
+					if (index < 0 || index >= ja.size())
 						throw new KIRuntimeException(
 						        StringFormatter.format("Index out of bound while accessing $", key));
 					el = ja.get(index);
+					mem = new JsonPrimitive(index);
 				} else {
 
-					String mem = je.getAsString();
 					el = el.getAsJsonObject()
-					        .get(mem);
+					        .get(mem.getAsString());
 				}
 			}
 		}
 
-		if (el == null) {
-			throw new KIRuntimeException(StringFormatter.format("Unable to set the context in the path $", key));
-		}
+		el = prev;
+		do {
 
-		Operation op = ops.removeLast();
-		ExpressionToken token = tokens.removeLast();
+			if (mem != null) {
+				JsonElement je = null;
+				if (op == Operation.OBJECT_OPERATOR) {
+					je = new JsonObject();
+				} else {
+					je = new JsonArray();
+				}
+
+				if (el.isJsonObject()) {
+					el.getAsJsonObject()
+					        .add(mem.getAsString(), je);
+				} else if (el.isJsonArray()) {
+					JsonArray arr = el.getAsJsonArray();
+					int ind = mem.getAsInt();
+					while (ind >= arr.size())
+						arr.add(JsonNull.INSTANCE);
+					arr.set(ind, je);
+				}
+				el = je;
+			}
+
+			if (!ops.isEmpty())
+				op = ops.removeLast();
+			if (!tokens.isEmpty()) {
+				token = tokens.removeLast();
+				mem = token instanceof ExpressionTokenValue etv ? etv.getElement()
+				        .getAsJsonPrimitive() : new JsonPrimitive(token.getExpression());
+			}
+
+		} while (!ops.isEmpty() && !tokens.isEmpty());
 
 		// TODO: Here I need to validate the schema of the value I have to put in the
 		// context.
 
-		if (op == Operation.OBJECT_OPERATOR) {
+		if (mem != null) {
 
-			if (!el.isJsonObject()) {
-				throw new KIRuntimeException(StringFormatter.format("$ has no object in the context", key));
-			}
-
-			String mem = null;
-			if (token instanceof ExpressionTokenValue etv)
-				mem = etv.getTokenValue()
-				        .getAsString();
-			else
-				mem = token.getExpression();
-
-			el.getAsJsonObject()
-			        .add(mem, value);
-		} else {
-			JsonElement je = token instanceof ExpressionTokenValue etv ? etv.getElement()
-			        : new JsonPrimitive(token.getExpression());
-
-			if (!je.isJsonPrimitive()) {
-				throw new KIRuntimeException(
-				        StringFormatter.format("Cannot extract json with key $ from the object $", je, el));
-			}
-
-			if (el.isJsonArray()) {
-
-				Tuple2<SchemaType, Object> prim = PrimitiveUtil.findPrimitive(je);
-
-				if (prim.getT1() != SchemaType.INTEGER && prim.getT1() != SchemaType.LONG) {
-					throw new KIRuntimeException(StringFormatter.format("Expecting a numerical index but found $", je));
-				}
-
-				int index = ((Number) prim.getT2()).intValue();
-				JsonArray ja = el.getAsJsonArray();
-
-				while (index >= ja.size())
-					ja.add(JsonNull.INSTANCE);
-				ja.set(index, value);
-			} else {
-
-				String mem = je.getAsString();
+			if (el.isJsonObject()) {
 				el.getAsJsonObject()
-				        .add(mem, value);
+				        .add(mem.getAsString(), value);
+			} else if (el.isJsonArray()) {
+				JsonArray arr = el.getAsJsonArray();
+				int ind = mem.getAsInt();
+				while (ind >= arr.size())
+					arr.add(JsonNull.INSTANCE);
+				arr.set(ind, value);
 			}
 		}
 
