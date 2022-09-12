5e230d3033708744211f311223750ab656ddbcce
serialcoder13 <kiran.grandhi@fincity.com>
2022-09-12 13:22:21 +0530

    Updated the statements with Override feature

--- kirun-js/src/engine/model/AbstractStatement.ts
+++ kirun-js/src/engine/model/AbstractStatement.ts
@@ -4,6 +4,7 @@ export class AbstractStatement {
     private comment?: string;
     private description?: string;
     private position?: Position;
+    private override: boolean = false;
 
     public getComment(): string | undefined {
         return this.comment;
@@ -12,6 +13,15 @@ export class AbstractStatement {
         this.comment = comment;
         return this;
     }
+    public isOverride(): boolean {
+        return this.override;
+    }
+
+    public setOverride(override: boolean): AbstractStatement {
+        this.override = override;
+        return this;
+    }
+
     public getDescription(): string | undefined {
         return this.description;
     }
