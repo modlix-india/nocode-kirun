856ff5ece2d700f38709c90bd70960430a09967b
Surendhar Satyavarapu <surendhar.s@fincity.com>
2023-03-16 11:09:40 +0530

    Filter and complex condition testcases

--- kirun-java/src/test/java/com/fincity/nocode/kirun/engine/json/schema/SchemaAnyofValidatorTest.java
+++ kirun-java/src/test/java/com/fincity/nocode/kirun/engine/json/schema/SchemaAnyofValidatorTest.java
@@ -186,11 +186,8 @@ class SchemaAnyofValidatorTest {
         assertEquals(bjob, res);
     }
 
-    // for 1 level complex condition schema is working but more than one level
-    // schema validation going wrong
-
     @Test
-    void filterComplexConditionTest() { // not working for self referencing
+    void filterComplexConditionTest() {
 
         Schema filterOperator = Schema.ofString("filterOperator").setNamespace("test")
                 .setEnums(List.of(new JsonPrimitive("EQUALS"),
@@ -288,6 +285,117 @@ class SchemaAnyofValidatorTest {
 
         System.out.println(bjob);
         var res = SchemaValidator.validate(null, ComplexCondition, repo, bjob);
+
+        assertEquals(bjob, res);
+
+    }
+
+    @Test
+    void filterAndComplexConditionTest() {
+
+        Schema filterOperator = Schema.ofString("filterOperator").setNamespace("test")
+                .setEnums(List.of(new JsonPrimitive("EQUALS"),
+                        new JsonPrimitive("LESS_THAN"), new JsonPrimitive("GREATER_THAN"),
+                        new JsonPrimitive("LESS_THAN_EQUAL"), new JsonPrimitive("BETWEEN"), new JsonPrimitive("IN")))
+                .setDefaultValue(new JsonPrimitive("EQUALS"));
+
+        Schema FilterCondition = Schema.ofObject("FilterCondition").setNamespace("test").setProperties(Map.of("negate",
+                Schema.ofBoolean("negate").setDefaultValue(new JsonPrimitive(Boolean.FALSE)), "operator",
+                Schema.ofRef("test.filterOperator"), "field",
+                Schema.ofString("field"),
+                "value", Schema.ofAny("value"), "toValue", Schema.ofAny("toValue"),
+
+                "multiValue",
+                Schema.ofArray("multiValue")
+                        .setItems(new ArraySchemaType().setSingleSchema(Schema.ofAny("singleType"))),
+                "isValue",
+                Schema.ofBoolean("isValue").setDefaultValue(new JsonPrimitive(false)), "isToValue",
+                Schema.ofBoolean("isToValue").setDefaultValue(new JsonPrimitive(false))))
+                .setRequired(List.of("operator", "field"))
+                .setAdditionalProperties(new AdditionalPropertiesType().setBooleanValue(false));
+
+        Schema complexOperator = Schema.ofString("complexOperator").setNamespace("test")
+                .setEnums(List.of(new JsonPrimitive("AND"), new JsonPrimitive("OR")));
+
+        Schema arraySchema = Schema.ofArray("conditions",
+                new Schema().setAnyOf(
+                        List.of(Schema.ofRef("#"), Schema.ofRef("test.FilterCondition"))));
+
+        Schema ComplexCondition = Schema.ofObject("ComplexCondition")
+                .setNamespace("test")
+                .setProperties(Map.of("conditions", arraySchema, "negate",
+                        Schema.ofBoolean("negate").setDefaultValue(new JsonPrimitive(Boolean.FALSE)),
+                        "operator", Schema.ofRef("test.complexOperator")))
+                .setRequired(List.of("conditions", "operator"))
+                .setAdditionalProperties(new AdditionalPropertiesType().setBooleanValue(false));
+
+        Schema conditions = Schema.ofObject("conditions").setNamespace("test")
+                .setProperties(Map.of("completeConditions", Schema.ofArray("completeConditions",
+                        Schema.of("singleSchema").setAnyOf(
+                                List.of(Schema.of("a").setRef("test.ComplexCondition"),
+                                        Schema.of("b").setRef("test.FilterCondition"))))));
+
+        var schemaMap = new HashMap<String, Schema>();
+
+        schemaMap.put("filterOperator", filterOperator);
+        schemaMap.put("FilterCondition", FilterCondition);
+        schemaMap.put("complexOperator", complexOperator);
+        schemaMap.put("ComplexCondition", ComplexCondition);
+        schemaMap.put("conditions", conditions);
+
+        class TestRepository implements Repository<Schema> {
+
+            @Override
+            public Schema find(String namespace, String name) {
+                if (namespace == null) {
+                    return null;
+                }
+                return schemaMap.get(name);
+            }
+
+        }
+        var repo = new HybridRepository<>(new TestRepository(), new KIRunSchemaRepository());
+
+        var tempOb = new JsonObject();
+        tempOb.addProperty("field", "a.b.c.d");
+        tempOb.addProperty("value", "surendhar");
+        tempOb.addProperty("operator", "LESS_THAN");
+        tempOb.addProperty("negate", true);
+        tempOb.addProperty("isValue", false);
+
+        var tempOb1 = new JsonObject();
+        tempOb1.addProperty("field", "a.b.c.d");
+        tempOb1.addProperty("value", "surendhar");
+        tempOb1.addProperty("operator", "GREATER_THAN");
+        tempOb1.addProperty("negate", true);
+        tempOb1.addProperty("isValue", true);
+
+        var jsonArrayI = new JsonArray();
+        jsonArrayI.add("a");
+        jsonArrayI.add("b");
+        jsonArrayI.add("c");
+
+        var tempOb2 = new JsonObject();
+        tempOb2.addProperty("field", "a.b.c.d");
+        tempOb2.add("multiValue", jsonArrayI);
+        tempOb2.addProperty("operator", "IN");
+
+        JsonObject mjob = new JsonObject();
+        JsonObject bjob = new JsonObject();
+        JsonArray ja = new JsonArray();
+        bjob.add("conditions", ja);
+        bjob.addProperty("negate", true);
+        bjob.addProperty("operator", "OR");
+        bjob.get("conditions").getAsJsonArray().add(tempOb);
+        bjob.get("conditions").getAsJsonArray().add(tempOb1);
+        bjob.get("conditions").getAsJsonArray().add(tempOb2);
+        mjob.add("conditions", new JsonArray());
+        mjob.addProperty("negate", false);
+        mjob.addProperty("operator", "AND");
+        bjob.get("conditions").getAsJsonArray().add(mjob);
+
+        System.out.println(bjob);
+        var res = SchemaValidator.validate(null, conditions, repo, bjob);
 
         assertEquals(bjob, res);
 
